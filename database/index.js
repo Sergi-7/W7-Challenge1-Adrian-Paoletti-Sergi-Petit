d101b35e3efdfd2698853188671fa10e23992faa
AdrianPaoletti <adriandelapao@gmail.com>
2021-11-10 18:46:27 +0100

    Added server and database folders

---
+++ database/index.js
@@ -0,0 +1,30 @@
+const debug = require("debug")("series:database");
+const mongoose = require("mongoose");
+
+const connectDB = (connectionString) =>
+  new Promise((resolve, reject) => {
+    mongoose.set("debug", true);
+    mongoose.set("toJSON", {
+      transform: (doc, ret) => {
+        // eslint-disable-next-line no-underscore-dangle
+        delete ret._id;
+        // eslint-disable-next-line no-underscore-dangle
+        delete ret._v;
+      },
+    })
+    mongoose.connect(connectionString, (error) => {
+      if (error) {
+        debug("No se ha podido iniciar la DB");
+        debug(error.message);
+        reject();
+        return;
+      }
+      debug("DB conected");
+      resolve();
+    });
+    mongoose.connection.on("close", () => {
+      debug("Desconectado de la base de datos")
+    })
+  });
+
+module.exports = connectDB;
---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const connectDB = require("./database/index");
+const initializeServer = require("./server/index")
+
+const port = process.env.SERVER_PORT ?? 4000;
+
+(async () => {
+  try {
+    await connectDB(process.env.MONGODB);
+    initializeServer(port);
+    console.log("success")
+  } catch (error) {
+    console.log("aloo")
+    process.exit(1)
+  }
+})();
